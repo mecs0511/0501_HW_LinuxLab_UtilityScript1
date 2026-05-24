89bb5adbdbca54eb31f85154fe9a71e2c43916c8
Monumental Presidential <233160212+mecs0511@users.noreply.github.com>
2026-05-23 20:24:47 -0400

    first commit

---
+++ backup.sh/Untitled-1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Starting backup process..."
+
+mkdir -p backup
+
+
+echo "This is a sample file." > sample.txt
+
+
+cp sample.txt backup/
+
+
+echo "File copied successfully."
+
